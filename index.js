0342fb4c5a9d07fc95ae0d36af2ce54d5afaecb3
Carlos <carlosvcerqueira@gmail.com>
2024-09-12 13:57:46 -0300

    adiciona metas abertas

--- index.js
+++ index.js
@@ -59,8 +59,25 @@ const metasRealizadas = async () => {
     }
 
     await select({
-        message: "Metas realizadas",
+        message: "Metas realizadas " + realizadas.length,
         choices: [...realizadas]
+    })
+}
+
+const metasAbertas = async () => {
+    const abertas = metas.filter((meta) => {
+        return meta.checked != true
+        // ou !meta.checked pra simplificar a linha, tbm ficaria como meta diferente de true
+    })
+
+    if (abertas.length == 0) {
+        console.log("Não existem metas abertas!")
+        return
+    }
+
+    await select({
+        message: "Metas abertas " + abertas.length,
+        choices: [...abertas]
     })
 }
 
@@ -84,6 +101,10 @@ const start = async () => {
                     value: "realizadas"
                 },
                 {
+                    name: "Metas abertas",
+                    value: "abertas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -100,6 +121,9 @@ const start = async () => {
                 break
             case "realizadas":
                 await metasRealizadas()
+                break
+            case "abertas":
+                await metasAbertas()
                 break
             case "sair":
                 console.log("Até a próxima!")
